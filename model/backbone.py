fix: Configs for estimation of standard deviation.

model/backbone.py
@@ -235,9 +235,9 @@ class SliceShort2(nn.Module):
             if with_debug:
                 x = self.conv_3(x)
 
-                debug = {"bb_mean_x_before_bn": x.abs().mean().item()}
+                debug = {"bb_mean_x_before_bn": x.abs().mean()}
                 x = self.bn3(x)
-                debug["bb_mean_x_after_bn"] = x.abs().mean().item()
+                debug["bb_mean_x_after_bn"] = x.abs().mean()
                 x = F.leaky_relu(x)
                 return x, debug
             else:
@@ -376,9 +376,9 @@ class BackboneNoSlice3(nn.Module):
             x = F.leaky_relu(self.bn_3(self.conv_3(x)))
             if with_debug:
                 x = self.conv_4(x)
-                debugs = {"bb_mean_x_before_bn": x.abs().mean().item()}
+                debugs = {"bb_mean_x_before_bn": x.abs().mean()}
                 x = self.bn_4(x)
-                debugs["bb_mean_x_after_bn"] = x.abs().mean().item()
+                debugs["bb_mean_x_after_bn"] = x.abs().mean()
                 x = F.leaky_relu(x)
                 return x, debugs
             else:
@@ -391,5 +391,69 @@ class BackboneNoSlice3(nn.Module):
             x = F.leaky_relu(self.conv_2(x))
             x = F.leaky_relu(self.conv_3(x))
             x = F.leaky_relu(self.conv_4(x))
+            return x
+
+
+# backboneNoSlice4 is similar to BackboneNoSlice3 but when downsampling the channels are split into a near and a far
+# receptive field near and far receptive field are concatenated at the output
+class BackboneNoSlice4(nn.Module):
+
+    @staticmethod
+    def radius():
+        return 12
+
+    def __init__(self, height=896, channels=[16, 32, 64], channels_sub=[64, 64, 64, 64], use_bn=False):
+        super(BackboneNoSlice4, self).__init__()
+        self.height = height
+        self.features = channels_sub[3]
+        self.conv_start = nn.Conv2d(1, channels[0], 3, padding=(1, 1)) # receptive field (radius) = 1
+        self.conv_1 = nn.Conv2d(channels[0], channels[1], 5, padding=(2, 2)) # + 2 = 3
+        self.conv_down = nn.Conv2d(channels[1], channels[2] * 2, 5, padding=(2, 2), stride=(2, 2)) # +2 = 5
+
+        self.conv_2 = nn.Conv2d(channels_sub[0]*2, channels_sub[1]*2, 5, padding=(2, 2), groups=2)  # + 2 * 2 = 9
+        self.conv_3 = nn.Conv2d(channels_sub[1], channels_sub[2], 5, padding=(2, 2))  # + 2 * 2 = 13
+        self.conv_4 = nn.Conv2d(channels_sub[2], channels_sub[3], 5, padding=(2, 2))  # + 2 * 2 = 17
+
+        if use_bn:
+            self.bn_start = nn.BatchNorm2d(channels[0])
+            self.bn_1 = nn.BatchNorm2d(channels[1])
+            self.bn_down = nn.BatchNorm2d(channels[2]*2)
+            self.bn_2 = nn.BatchNorm2d(channels_sub[1]*2)
+            self.bn_3 = nn.BatchNorm2d(channels_sub[2])
+            self.bn_4 = nn.BatchNorm2d(channels_sub[3])
+
+    def forward(self, x, with_debug=False):
+        device = x.device
+        #print(nr_slices)
+        #print(x.shape)
+        if hasattr(self, 'bn_start'):
+            x = F.leaky_relu(self.bn_start(self.conv_start(x)))
+            x = F.leaky_relu(self.bn_1(self.conv_1(x)))
+            x = F.leaky_relu(self.bn_down(self.conv_down(x)))
+            x = F.leaky_relu(self.bn_2(self.conv_2(x)))
+
+            x, x_l = torch.split(x, int(x.shape[1]/2), 1)# split up the channels in two
+            x = F.leaky_relu(self.bn_3(self.conv_3(x)))
+            if with_debug:
+                x = self.conv_4(x)
+                debugs = {"bb_mean_x_before_bn": x.abs().mean()}
+                x = self.bn_4(x)
+                debugs["bb_mean_x_after_bn"] = x.abs().mean()
+                x = F.leaky_relu(x)
+                x = torch.cat((x, x_l), 1)
+                return x, debugs
+            else:
+                x = F.leaky_relu(self.bn_4(self.conv_4(x)))
+                x = torch.cat((x, x_l), 1)
+                return x
+        else:
+            x = F.leaky_relu(self.conv_start(x))
+            x = F.leaky_relu(self.conv_1(x))
+            x = F.leaky_relu(self.conv_down(x))
+            x = F.leaky_relu(self.conv_2(x))
+            x, x_l = torch.split(x, int(x.shape[1]/2), 1)# split up the output in two
+            x = F.leaky_relu(self.conv_3(x))
+            x = F.leaky_relu(self.conv_4(x))
+            x = torch.cat((x, x_l), 1)
             return x
 

model/regressor.py
@@ -229,15 +229,15 @@ class Classifier3Stage(nn.Module):
         mean_weights = {}
         accu = 0
         for c in self.c1:
-            accu += c.weight.abs().mean().item()
+            accu += c.weight.abs().mean()
         mean_weights["mean_weight_c1"] = accu
         accu = 0
         for c in self.c2:
-            accu += c.w.abs().mean().item()
+            accu += c.w.abs().mean()
         mean_weights["mean_weight_c2"] = accu
         accu = 0
         for c in self.c3:
-            accu += c.w.abs().mean().item()
+            accu += c.w.abs().mean()
         mean_weights["mean_weight_c3"] = accu
         return mean_weights
 
@@ -351,7 +351,7 @@ class Classifier3Stage(nn.Module):
                 x = x.reshape((bs, height, width, -1)).permute((0, 3, 1, 2))
                 loss = F.cross_entropy(x, inds2_gt) * mask
                 losses.append(loss.mean())
-                torch.cuda.synchronize()
+                #torch.cuda.synchronize()
 
             inds12_gt = inds_gt // self.classes[2]
             inds12_gt = inds12_gt
@@ -383,7 +383,7 @@ class Classifier3Stage(nn.Module):
                 x = x.reshape((bs, height, width, -1)).permute((0, 3, 1, 2))
                 loss = F.cross_entropy(x, inds3_gt) * mask
                 losses.append(loss.mean())
-                torch.cuda.synchronize()
+                #torch.cuda.synchronize()
 
             return inds123_real, losses
 
@@ -549,11 +549,11 @@ class Reg_3stage(nn.Module):
 
             x = (inds.type(torch.float32) + r) * (1.0 / float(classes123))
             x_real = x.reshape((bs, 1, height, width))
-            torch.cuda.synchronize()
+            #torch.cuda.synchronize()
 
             #lets check if the weights
             debugs = self.c.get_mean_weights()
-            debug_r = self.r2.w.abs().mean().item() + self.r3.w.abs().mean().item()
+            debug_r = self.r2.w.abs().mean() + self.r3.w.abs().mean()
             debugs["mean_w_reg"] = debug_r
 
 

plot_results_real.py
@@ -52,6 +52,9 @@ input_height = 2*17+1
 
 regressor = "trained_models/bb64_256c_16sc_256_8_lr02_regressor_chk.pt"
 backbone = "trained_models/bb64_256c_16sc_256_8_lr02_backbone_chk.pt"
+
+regressor = "trained_models/slice_2stage_class_31_regressor_chk.pt"
+backbone = "trained_models/slice_2stage_class_31_backbone_chk.pt"
 input_height = 128
 
 sigma_estimator = "trained_models/sigma_mask_scaled_chk.pt"
@@ -78,10 +81,10 @@ dataset = DatasetCaptured(root_dir=dataset_path, from_ind=0, to_ind=800)
 #dataset_path = "/media/simon/ssd_data/data/datasets/structure_core_unity"
 #dataset = DatasetRendered2(dataset_path, 0, 40000, tgt_res=tgt_res)
 
-fig, axs = plt.subplots(4, 1)
+fig, axs = plt.subplots(5, 1)
 for i, ir in enumerate(dataset):
-    bl = baselines[i%2]
-    #bl = -baselines[1]# Did i mix up left and right when creating the dataset?
+    #bl = baselines[i%2]
+    bl = -baselines[1]# Did i mix up left and right when creating the dataset?
     with torch.no_grad():
         ir = torch.tensor(ir, device=device).unsqueeze(0)
         #ir = ir[:, :, 100+1:(100+17*2+1)+1, :]
@@ -115,10 +118,15 @@ for i, ir in enumerate(dataset):
         axs[2].set_title('Depth single line')
         axs[2].plot(d[0, 0, :].squeeze())
 
-        #todo: the depth estimaion!
         axs[3].cla()
-        axs[3].set_title('Depth')
-        axs[3].imshow(d[0, :, :])
+        axs[3].set_title('sigma')
+        axs[3].plot(mask[0, 0, :].squeeze())
+
+        #todo: the depth estimaion!
+        axs[4].cla()
+        axs[4].set_title('Depth')
+        axs[4].imshow(d[0, :, :])
+
 
         plt.show(block=False)
         plt.waitforbuttonpress()

train.py
@@ -13,7 +13,7 @@ from model.regressor_1branch import Regressor1Branch
 from model.regressor_branchless import RegressorBranchless
 from model.backbone_6_64 import Backbone6_64
 from experiments.lines.model_lines_CR8_n import *
-from model.backbone import Backbone, BackboneSliced, BackboneSliced2, BackboneSliced3, BackboneNoSlice3
+from model.backbone import Backbone, BackboneSliced, BackboneSliced2, BackboneSliced3, BackboneNoSlice3, BackboneNoSlice4
 from model.regressor import Regressor, Regressor2, Reg_3stage
 from torch.utils.data import DataLoader
 import math
@@ -79,24 +79,28 @@ def sigma_loss(sigma, x, x_gt):  # sigma_sq is also called "variance"
         print("sigma: found nan")
     if torch.any(torch.isinf(sigma)):
         print("sigma: found inf")
-    eps = 0.01  # sqrt(0.00001) = 0.0003 ... that means we actually have approx 0.3 pixel of basic offset for sigma
-    term1 = torch.div(torch.square((x - x_gt) * 1024.0), sigma * sigma + eps)
+    #ideally this woudl be 0.001 so we are far away from the preferred precision of 0.1 pixel (0.1*0.1 = 0.01)
+    eps = 0.001  # sqrt(0.00001) = 0.0003 ... that means we actually have approx 0.3 pixel of basic offset for sigma
+    #print(f"max disparity error {torch.max(torch.abs(x-x_gt))}")
+    term1 = torch.div(torch.square(x - x_gt).clamp(0, 1) * (1024.0*1024.0), sigma * sigma + eps)
     term2 = torch.log(sigma * sigma + eps)
+    sigma_sq = sigma * sigma
     # term 3 is to stay positive(after all it is sigma^2)
     term3 = 0  # F.relu(-sigma)
     loss = term1 + term2 + term3
-    # print("term1")
-    # print(term1)
-    # print("term2")
-    # print(term2)
-    # print("max term1")
-    # print(torch.max(term1))
-    # print("max term2")
-    # print(torch.max(term2))
-    # print("max sigma:")
-    # print(torch.max(torch.abs(sigma_sq)))
-    # print("term 1 den:")
-    # print((2*torch.min(torch.abs(sigma_sq)) + eps))
+    #print("term1")
+    #print(term1)
+    #print("term2")
+    #print(term2)
+    #print("max term1")
+    #print(torch.max(term1))
+    #print("max term2")
+    #print(torch.max(term2))
+    #print("max sigma:")
+    #print(torch.max(torch.abs(sigma_sq)))
+    #print("term 1 den:")
+    #print((2*torch.min(torch.abs(sigma_sq)) + eps))
+    #print(f"maximum loss term: {torch.max(loss)}")
     if torch.any(torch.isnan(term1)):
         print("term1: found nan")
     if torch.any(torch.isinf(term1)):
@@ -175,10 +179,12 @@ def train():
     parser.add_argument("-a", "--alpha_reg", dest="alpha_reg", action="store",
                         help="The factor with which the regression error is incorporated into the loss.",
                         type=float,
+                        nargs="+",
                         default=config["training"]["alpha_reg"])
     parser.add_argument("-as", "--alpha_sigma", dest="alpha_sigma", action="store",
                         help="The factor with which mask error is incorporated into the loss.",
                         type=float,
+                        nargs="+",
                         default=config["training"]["alpha_sigma"])
 
     args = parser.parse_args(additional_args)
@@ -243,6 +249,10 @@ def train():
                                         channels=config["backbone"]["channels"],
                                         channels_sub=config["backbone"]["channels2"],
                                         use_bn=True)
+            #backbone = BackboneNoSlice3(height=config["dataset"]["slice_in"]["height"],
+            #                            channels=config["backbone"]["channels"],
+            #                            channels_sub=config["backbone"]["channels2"],
+            #                            use_bn=True)
             #backbone = BackboneSliced3(slices=1, height=config["dataset"]["slice_in"]["height"],
             #                           channels=config["backbone"]["channels"],
             #                           channels_sub=config["backbone"]["channels2"],
@@ -281,10 +291,16 @@ def train():
         lr_scales = [1.0]
     key_steps.insert(0, -1)
 
+    #assert
     if isinstance(args.alpha_reg, list):
         alpha_regs = args.alpha_reg
     else:
         alpha_regs = [args.alpha_reg] * len(key_steps)
+
+    if isinstance(args.alpha_sigma, list):
+        alpha_sigmas = args.alpha_sigma
+    else:
+        alpha_sigmas = [args.alpha_sigma] * len(key_steps)
 
 
     def find_index(epoch, key_steps):
@@ -318,14 +334,15 @@ def train():
     step = -1
     for epoch in range(1, args.epochs):
         # TODO: setup code like so: https://pytorch.org/tutorials/beginner/transfer_learning_tutorial.html
-        print('Epoch {}/{}'.format(epoch, args.epochs - 1))
+        print(f'Epoch {epoch}/{args.epochs - 1}')
         print('-' * 10)
         alpha_reg = alpha_regs[find_index(epoch-1, key_steps)]
+        alpha_sigma = float(alpha_sigmas[find_index(epoch-1, key_steps)])
 
         def get_lr(optimizer):
             for param_group in optimizer.param_groups:
                 return param_group['lr']
-        print(f"alpha_reg {alpha_reg}, learning_rate {get_lr(optimizer)}")
+        print(f"alpha_reg {alpha_reg}, alpha_sigma {alpha_sigma}, learning_rate {get_lr(optimizer)}")
 
         for phase in ['train', 'val']:
             print(phase)
@@ -364,9 +381,10 @@ def train():
                     x, sigma, class_losses, x_real, debug = model(ir, x_gt, mask_gt)
                     x_real = x_real.detach()
 
-                    debug_names = ["c1", "c2", "c3", "r", "mean_backbone_output"]
-                    for key, value in debug.items():
-                        writer.add_scalar(f'debug/{key}', value, step)
+                    # log weights and activations of different layers
+                    if False:
+                        for key, value in debug.items():
+                            writer.add_scalar(f'debug/{key}', value.item(), step)
 
                     optimizer.zero_grad()
                     loss = torch.mean(torch.abs(x - x_gt))  # mask_gt
@@ -377,11 +395,11 @@ def train():
 
                     loss = loss * alpha_reg
 
-                    if args.alpha_sigma != 0.0:
+                    if alpha_sigma != 0.0:
                         loss_sigma = sigma_loss(sigma, x_real, x_gt)
                         loss_sigma_acc += loss_sigma.item()
                         loss_sigma_acc_sub += loss_sigma.item()
-                        loss += loss_sigma * args.alpha_sigma
+                        loss += loss_sigma * alpha_sigma
 
                     if len(loss_class_acc) == 0:
                         loss_class_acc = [0] * len(class_losses)
@@ -411,8 +429,8 @@ def train():
                         # loss_disparity_acc += loss.item()
                         # loss_disparity_acc_sub += loss.item()
 
-                        if args.alpha_sigma != 0.0:
-                            loss_sigma = sigma_loss(sigma, x, x_gt)
+                        if alpha_sigma != 0.0:
+                            loss_sigma = sigma_loss(sigma, x_real, x_gt)
                             loss_sigma_acc += loss_sigma.item()
                             loss_sigma_acc_sub += loss_sigma.item()
 
@@ -424,7 +442,7 @@ def train():
                 if i_batch % 100 == 99:
                     writer.add_scalar(f'{phase}_subepoch/disparity_error',
                                       loss_disparity_acc_sub / 100.0 * 1024, step)
-                    if args.alpha_sigma != 0.0:
+                    if alpha_sigma != 0.0:
                         writer.add_scalar(f'{phase}_subepoch/sigma_loss',
                                           loss_sigma_acc_sub / 100.0, step)
 
@@ -432,7 +450,7 @@ def train():
                         writer.add_scalar(f'{phase}_subepoch/regression_error', loss_reg_acc_sub / 100.0 * 1024, step)
 
                         combo_loss = loss_reg_acc_sub * alpha_reg + \
-                                     loss_sigma_acc_sub * args.alpha_sigma + sum(loss_class_acc_sub)
+                                     loss_sigma_acc_sub * alpha_sigma + sum(loss_class_acc_sub)
                         print("batch {} loss: {}".format(i_batch, combo_loss / 100))
                     else:  # val
                         print("batch {} disparity error: {}".format(i_batch, loss_disparity_acc_sub / 100 * 1024))
@@ -447,7 +465,7 @@ def train():
             #write progress every epoch!
             writer.add_scalar(f"{phase}/disparity",
                               loss_disparity_acc / dataset_sizes[phase] * args.batch_size * 1024, step)
-            if args.alpha_sigma != 0.0:
+            if alpha_sigma != 0.0:
                 writer.add_scalar(f"{phase}/sigma(loss)",
                                   loss_sigma_acc / dataset_sizes[phase] * args.batch_size, step)
             for i, class_loss in enumerate(loss_class_acc):
@@ -456,7 +474,7 @@ def train():
             if phase == 'train':
                 writer.add_scalar(f"{phase}/regression_stage",
                                   loss_reg_acc / dataset_sizes[phase] * args.batch_size * 1024, step)
-                combo_loss = loss_reg_acc * alpha_reg + loss_sigma_acc * args.alpha_sigma + sum(loss_class_acc_sub)
+                combo_loss = loss_reg_acc * alpha_reg + loss_sigma_acc * alpha_sigma + sum(loss_class_acc_sub)
                 combo_loss *= 1.0 / dataset_sizes[phase] * args.batch_size
 
                 print(f"{phase} loss: {combo_loss}")