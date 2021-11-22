89715b5758a80e350132cf97e9cb858f09676348
Simon Schreiberhuber <simon.schreiberhuber@gmx.net>
2021-11-22 15:42:35 +0100

    feat: Config files for low-jitter experiments.

--- benchmarking/apply_to_structure_core.py
+++ benchmarking/apply_to_structure_core.py
@@ -48,8 +48,8 @@ backbone_model_pth = "trained_models/full_66_lcn_j4_domain_shift_v2_backbone_chk
 gressor_model_pth = "trained_models/full_66_lcn_j4_light_regressor_chk.pt"
 backbone_model_pth = "trained_models/full_66_lcn_j4_light_backbone_chk.pt"
 
-regressor_model_pth = "trained_models/full_66_lcn_j4_regressor_chk.pt"
-backbone_model_pth = "trained_models/full_66_lcn_j4_backbone_chk.pt"
+#regressor_model_pth = "trained_models/full_66_lcn_j4_regressor_chk.pt"
+#backbone_model_pth = "trained_models/full_66_lcn_j4_backbone_chk.pt"
 
 
 device = "cuda:0"
