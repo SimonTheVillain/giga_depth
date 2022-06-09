e0bf0fe72f85b5d0cba46fd19fe58855f6c7be11
Simon <simon.schreiberhuber@gmx.net>
2022-06-09 19:22:27 +0200

    feat new training config files.

---
+++ multi-train.sh
@@ -0,0 +1,11 @@
+python3 train.py --dataset_path=/media/simon/ssd_datasets/datasets/structure_core_unity_sequences --config_file configs/full_77_lcn_j2_c1920.yaml
+python3 train.py --dataset_path=/media/simon/ssd_datasets/datasets/structure_core_unity_sequences --config_file configs/full_72_unet.yaml
+python3 train.py --dataset_path=/media/simon/ssd_datasets/datasets/structure_core_unity_sequences --config_file configs/full_76_lcn_j2_c288.yaml
+python3 train.py --dataset_path=/media/simon/ssd_datasets/datasets/structure_core_unity_sequences --config_file configs/full_76_lcn_j2_c384.yaml
+python3 train.py --dataset_path=/media/simon/ssd_datasets/datasets/structure_core_unity_sequences --config_file configs/full_76_lcn_j2_c640_r1.yaml
+python3 train.py --dataset_path=/media/simon/ssd_datasets/datasets/structure_core_unity_sequences --config_file configs/full_76_lcn_j2_c640_r2.yaml
+python3 train.py --dataset_path=/media/simon/ssd_datasets/datasets/structure_core_unity_sequences --config_file configs/full_76_lcn_j2_c640_r3.yaml
+python3 train.py --dataset_path=/media/simon/ssd_datasets/datasets/structure_core_unity_sequences --config_file configs/full_76_lcn_j2_c1280.yaml
+python3 train.py --dataset_path=/media/simon/ssd_datasets/datasets/structure_core_unity_sequences --config_file configs/full_76_lcn_j2_c1536.yaml
+python3 train.py --dataset_path=/media/simon/ssd_datasets/datasets/structure_core_unity_sequences --config_file configs/full_76_lcn_j2_c1920.yaml
+python3 train.py --dataset_path=/media/simon/ssd_datasets/datasets/structure_core_unity_sequences --config_file configs/full_76_lcn_j2_c2688.yaml
