ceb78a3f2da5229e09ac46c437afdf806998ca17
Simon <simon.schreiberhuber@gmx.net>
2022-06-15 21:31:13 +0200

    fix: add a non-lcn experiment to the mix.

--- multi-train.sh
+++ multi-train.sh
@@ -1,6 +1,6 @@
-python3 train.py --dataset_path=/media/simon/ina/datasets/DepthInSpace/rendered_default --config_file configs/dis_def_lcn_c1920.yaml
 python3 train.py --dataset_path=/media/simon/ina/datasets/DepthInSpace/rendered_default --config_file configs/dis_kin_lcn_c1920.yaml
 python3 train.py --dataset_path=/media/simon/ina/datasets/DepthInSpace/rendered_default --config_file configs/dis_real_lcn_c1920.yaml
 python3 train.py --dataset_path=/media/simon/ina/datasets/DepthInSpace/rendered_default --config_file configs/dis_real_lcn_j2_c1920.yaml
 python3 train.py --dataset_path=/media/simon/ssd_datasets/datasets/structure_core_unity_sequences --config_file configs/tuning/full_76_lcn_j2_c2688.yaml
+python3 train.py --dataset_path=/media/simon/ssd_datasets/datasets/structure_core_unity_sequences --config_file configs/tuning/full_76_j2_c1920.yaml
 python3 train.py --dataset_path=/media/simon/ssd_datasets/datasets/structure_core_unity_sequences --config_file configs/tuning/full_78_unet_j2_c1920.yaml
