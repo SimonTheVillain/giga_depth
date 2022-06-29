72cf11e3e8475b84fc8198b3106db04d4b0a9b9d
Simon Schreiberhuber <simon.schreiberhuber@gmx.net>
2022-06-29 08:51:33 +0200

    added gradient loss.

--- train_ambient_estimator.py
+++ train_ambient_estimator.py
@@ -9,7 +9,64 @@ from dataset.dataset_captured_ambient import DatasetCapturedAmbient
 import cv2
 from common.common import LCN_tensors
 import math
+import torch.nn as nn
+import torch.nn.functional as F
 
+
+class GradLayer(nn.Module):
+
+    def __init__(self):
+        super(GradLayer, self).__init__()
+        kernel_v = [[0, -1, 0],
+                    [0, 0, 0],
+                    [0, 1, 0]]
+        kernel_h = [[0, 0, 0],
+                    [-1, 0, 1],
+                    [0, 0, 0]]
+        kernel_h = torch.FloatTensor(kernel_h).unsqueeze(0).unsqueeze(0)
+        kernel_v = torch.FloatTensor(kernel_v).unsqueeze(0).unsqueeze(0)
+        self.weight_h = nn.Parameter(data=kernel_h, requires_grad=False)
+        self.weight_v = nn.Parameter(data=kernel_v, requires_grad=False)
+
+    def get_gray(self,x):
+        '''
+        Convert image to its gray one.
+        '''
+        gray_coeffs = [65.738, 129.057, 25.064]
+        convert = x.new_tensor(gray_coeffs).view(1, 3, 1, 1) / 256
+        x_gray = x.mul(convert).sum(dim=1)
+        return x_gray.unsqueeze(1)
+
+    def forward(self, x):
+        # x_list = []
+        # for i in range(x.shape[1]):
+        #     x_i = x[:, i]
+        #     x_i_v = F.conv2d(x_i.unsqueeze(1), self.weight_v, padding=1)
+        #     x_i_h = F.conv2d(x_i.unsqueeze(1), self.weight_h, padding=1)
+        #     x_i = torch.sqrt(torch.pow(x_i_v, 2) + torch.pow(x_i_h, 2) + 1e-6)
+        #     x_list.append(x_i)
+
+        # x = torch.cat(x_list, dim=1)
+        if x.shape[1] == 3:
+            x = self.get_gray(x)
+
+        x_v = F.conv2d(x, self.weight_v, padding=1)
+        x_h = F.conv2d(x, self.weight_h, padding=1)
+        x = torch.sqrt(torch.pow(x_v, 2) + torch.pow(x_h, 2) + 1e-6)
+
+        return x
+
+class GradLoss(nn.Module):
+
+    def __init__(self):
+        super(GradLoss, self).__init__()
+        self.loss = nn.L1Loss()
+        self.grad_layer = GradLayer()
+
+    def forward(self, output, gt_img):
+        output_grad = self.grad_layer(output)
+        gt_grad = self.grad_layer(gt_img)
+        return self.loss(output_grad, gt_grad)
 
 def train():
     experiment_name = "amb_estimator2"
@@ -24,10 +81,12 @@ def train():
     epochs = 10
     dataset_path = "/home/simon/datasets/structure_core/sequences_combined_ambient"
     weight_LCN_loss = 1.0
+    weight_sobel = 1.0
     writer = SummaryWriter(f"tensorboard/{experiment_name}")
 
     model = UNet(1, 1, channel_size_scale=0.25)
     model.cuda()
+    loss_sobel = GradLoss().cuda()
     model = torch.load("trained_models/amb_estimator2.pt")
     model.eval()
     model.cuda()
@@ -100,6 +159,7 @@ def train():
                     # lcn1 = LCN_tensors(ambient_gt)[0]
                     # cv2.imshow("lcn_gt", lcn1[0,0,:,:].detach().cpu().numpy()*0.1+0.5)
                     # cv2.waitKey()
+                    loss += weight_sobel * loss_sobel(ambient_est, ambient_gt)
                     loss += weight_LCN_loss * torch.abs(LCN_tensors(ambient_est)[0] - LCN_tensors(ambient_gt)[0]).mean()
 
                     if False:
@@ -129,6 +189,7 @@ def train():
                     delta = torch.abs(ambient_est - ambient_gt).mean()
                     loss = delta.mean()
                     loss += (delta*delta).mean()
+                    loss += weight_sobel * loss_sobel(ambient_est, ambient_gt)
                     loss += weight_LCN_loss * torch.abs(
                         LCN_tensors(ambient_est.type(torch.float32))[0] - LCN_tensors(ambient_gt)[0]).mean()
 
